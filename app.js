1c7528fcceb175ff30877369a0a55507676edf72
conradtsamuel@gmail.com <conradtsamuel@gmail.com>
2024-10-08 21:49:36 -0300

    1.0.2

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var fs = require('fs'); // Módulo para ler arquivos do sistema
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+// Configurações de view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -19,21 +20,46 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Definindo rotas padrão
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// Função para gerar rotas automáticas com base nos arquivos da pasta views
+function generateRoutesFromViews() {
+  const viewsDir = path.join(__dirname, 'views');
+  
+  fs.readdir(viewsDir, (err, files) => {
+    if (err) {
+      console.error('Erro ao ler a pasta views:', err);
+      return;
+    }
+    
+    files.forEach(file => {
+      const routeName = `/${file.replace('.ejs', '')}`;
+      const viewName = file.replace('.ejs', '');
+      
+      app.get(routeName, (req, res) => {
+        res.render(viewName); 
+      });
+      
+      console.log(`Rota criada: ${routeName}`);
+    });
+  });
+}
+
+// Gera as rotas automaticamente
+generateRoutesFromViews();
+
+// Captura de erros 404
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Manipulador de erros
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
